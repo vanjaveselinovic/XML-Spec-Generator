dc66ce46eab7ed6f5d518c6b9ab3a6d291bbff18
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2016-08-25 16:28:14 -0400

    add file upload part

--- script.js
+++ script.js
@@ -39,96 +39,103 @@ $(document).ready(function () {
 		}
 	});
 
-/* STEP 1 Generate */
+	/* STEP 1 Generate */
 
-/* STEP 2 Build */
+	$('#rule-xml').change(function () {
+		if($('#rule-xml').val() !== '') $('#pp-upload').css('display', 'none');
+		else $('#pp-upload').css('display', 'block');
+	});
 
-var deletedAttributes = [];
+	/* STEP 2 Build */
 
-function getAttributes(oDOM) {
-	var attributes = [];
+	var deletedAttributes = [];
 
-	var elements = oDOM.getElementsByTagName("*");
+	function getAttributes(oDOM) {
+		var attributes = [];
 
-	var element;
-	for (var i = 0; i < elements.length; i++) {
+		var elements = oDOM.getElementsByTagName("*");
 
-		element = $(elements[i])[0];
-		if (element.children.length === 0) {
-			attributes.push({
-				tag: element.tagName,
-				val: element.textContent
-			});
+		var element;
+		for (var i = 0; i < elements.length; i++) {
+
+			element = $(elements[i])[0];
+			if (element.children.length === 0) {
+				attributes.push({
+					tag: element.tagName,
+					val: element.textContent
+				});
+			}
+		}
+
+		return attributes;
+	}
+
+	function apRow(tag, val) {
+		return '<div class="ap-row"><div class="ap-col ap-col-tag"><input type="text" value="'+tag+'"></div><div class="ap-col ap-col-val"><input type="text" value="'+val+'"><i class="material-icons no-highlight">delete</i></div></div>';
+	}
+
+	function addApRow(tag, val) {
+		$('#ap-main').append(apRow(tag, val));
+
+		$('.ap-row:last-of-type .ap-col-val i').click(function (ev) {
+			var el = $(ev.target);
+
+			deletedAttributes.push(apRow(el[0].parentElement.previousSibling.children[0].text, el[0].previousSibling.text));
+
+			$(el[0].parentElement.parentElement).remove();
+
+			ga('send', 'event', 'Step 2 Build', 'click', 'remove attribute');
+		});
+	};
+
+	$('#generate-button').click(function () {
+
+		var oParser = new DOMParser();
+		var oDOM = oParser.parseFromString($('#rule-xml').val().trim(), "text/xml");
+
+		if ($('#rule-name').val() !== ''
+			&& $('#rule-desc').val() !== ''
+			&& $(oDOM.documentElement)[0].outerHTML.indexOf('parsererror') === -1) {
+
+			$('#attributes-panel').removeClass('disabled-div');
+
+		var attributes = getAttributes(oDOM);
+
+		for(var i = 0; i < attributes.length; i++) {
+			addApRow(attributes[i]['tag'], attributes[i]['val']);
+		}
+
+		ga('send', 'event', 'Step 2 Build', 'generation', 'success');
+	}
+	else {
+		if($('#rule-name').val() === '') {
+			$('#rule-name').addClass('invalid-input');
+			ga('send', 'event', 'Step 2 Build', 'generation', 'failure - no title');
+		}
+		if ($('#rule-desc').val() === '') {
+			$('#rule-desc').addClass('invalid-input');
+			ga('send', 'event', 'Step 2 Build', 'generation', 'failure - no description');
+		}
+		if ($(oDOM.documentElement)[0].outerHTML.indexOf('parsererror') !== -1) {
+			$('#rule-xml').addClass('invalid-input');
+			ga('send', 'event', 'Step 2 Build', 'generation', 'failure - no/invalid xml');
 		}
 	}
 
-	return attributes;
-}
-
-function apRow(tag, val) {
-	return '<div class="ap-row"><div class="ap-col ap-col-tag"><input type="text" value="'+tag+'"></div><div class="ap-col ap-col-val"><input type="text" value="'+val+'"><i class="material-icons no-highlight">delete</i></div></div>';
-}
-
-function addApRow(tag, val) {
-	$('#ap-main').append(apRow(tag, val));
-	
-	$('.ap-row:last-of-type .ap-col-val i').click(function (ev) {
-		var el = $(ev.target);
-
-		deletedAttributes.push(apRow(el[0].parentElement.previousSibling.children[0].text, el[0].previousSibling.text));
-
-		$(el[0].parentElement.parentElement).remove();
-
-		ga('send', 'event', 'Step 2 Build', 'click', 'remove attribute');
+	ga('send', 'event', 'Step 2 Build', 'click', 'generate attributes');
 	});
-};
 
-$('#generate-button').click(function () {
+	$('#ap-footer').click(function () {
+		addApRow('', '');
 
-	var oParser = new DOMParser();
-	var oDOM = oParser.parseFromString($('#rule-xml').val().trim(), "text/xml");
+		var apMainViewport = document.getElementById("ap-main");
+		apMainViewport.scrollTop = apMainViewport.scrollHeight;
+	});
 
-	if ($('#rule-name').val() !== ''
-		&& $('#rule-desc').val() !== ''
-		&& $(oDOM.documentElement)[0].outerHTML.indexOf('parsererror') === -1) {
+	/* STEP 3 Test */
 
-		$('#attributes-panel').removeClass('disabled-div');
-
-	var attributes = getAttributes(oDOM);
-
-	for(var i = 0; i < attributes.length; i++) {
-		addApRow(attributes[i]['tag'], attributes[i]['val']);
-	}
-
-	ga('send', 'event', 'Step 2 Build', 'generation', 'success');
-}
-else {
-	if($('#rule-name').val() === '') {
-		$('#rule-name').addClass('invalid-input');
-		ga('send', 'event', 'Step 2 Build', 'generation', 'failure - no title');
-	}
-	if ($('#rule-desc').val() === '') {
-		$('#rule-desc').addClass('invalid-input');
-		ga('send', 'event', 'Step 2 Build', 'generation', 'failure - no description');
-	}
-	if ($(oDOM.documentElement)[0].outerHTML.indexOf('parsererror') !== -1) {
-		$('#rule-xml').addClass('invalid-input');
-		ga('send', 'event', 'Step 2 Build', 'generation', 'failure - no/invalid xml');
-	}
-}
-
-ga('send', 'event', 'Step 2 Build', 'click', 'generate attributes');
-});
-
-$('#ap-footer').click(function () {
-	addApRow('', '');
-
-	var apMainViewport = document.getElementById("ap-main");
-	apMainViewport.scrollTop = apMainViewport.scrollHeight;
-});
-
-/* STEP 3 Test */
-
-/* STEP 4 Submit */
+	/* STEP 4 Submit */
 
 });
+
+
