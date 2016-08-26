9c9bbe217e0a45b666ea004acd8ea51ca6388156
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2016-08-26 16:29:08 -0400

    fix

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ $(document).ready(function () {
     	window['miLib'] = miLib; // debug
     	mi = new miLib.MediaInfo();
 
-    	$('#generate-button').attr('disabled', 'false');
+    	$('#generate-button').prop('disabled', false);
 	});
 
 	$('#generate-button').click(function () {
