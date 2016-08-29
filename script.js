62875ff3f83c860680a196d5cd8eccca115cfce4
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2016-08-29 11:02:36 -0400

    page now still works if mediainfo fails to start, xml paste only

--- script.js
+++ script.js
@@ -178,6 +178,7 @@ $(document).ready(function () {
     	mi = new miLib.MediaInfo();
 
     	$('#pp-file').removeAttr('disabled');
+    	$('#pp-upload').removeClass('disabled-div');
 	});
 
 	$('#generate-button').click(function () {
